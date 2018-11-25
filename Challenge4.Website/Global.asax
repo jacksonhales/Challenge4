2d912eff91abdfeba4e6fe456d089b450c1a8e94
Jackson Hales <me@jacksonhales.com.au>
2018-11-26 08:25:12 +1100

    Initial Commit

---
+++ Challenge4.Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Challenge4.Website.MvcApplication" Language="C#" %>
---
+++ Challenge4.Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Challenge4.Website.Startup))]
+namespace Challenge4.Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
